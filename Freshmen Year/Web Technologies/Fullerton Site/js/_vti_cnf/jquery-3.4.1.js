a13a6796be5cd99a838d2a368405a26be6370382
Jula777-777 <juliadady7@gmail.com>
2023-09-23 09:48:26 -0700

    First Project Upload

---
+++ Freshmen Year/Web Technologies/Fullerton Site/js/_vti_cnf/jquery-3.4.1.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|07 Dec 2019 07:20:45 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|JUSTIN-COMPUTER\\justi
+vti_modifiedby:SR|JUSTIN-COMPUTER\\justi
+vti_timecreated:TR|07 Dec 2019 07:20:45 -0000
+vti_cacheddtm:TX|07 Dec 2019 07:20:45 -0000
+vti_filesize:IR|280364
+vti_backlinkinfo:VX|
---
+++ Freshmen Year/Web Technologies/Fullerton Site/js/_vti_cnf/main.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|JUSTIN-COMPUTER\\justi
+vti_modifiedby:SR|JUSTIN-COMPUTER\\justi
+vti_timelastmodified:TR|08 Dec 2019 01:02:53 -0000
+vti_timecreated:TR|08 Dec 2019 01:02:46 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|contact.html products.html services.html slider.dwt team.html about.html index.html
+vti_cacheddtm:TX|08 Dec 2019 01:02:53 -0000
+vti_filesize:IR|2616
+vti_nexttolasttimemodified:TR|08 Dec 2019 01:02:46 -0000
